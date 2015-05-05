76222f235c9bb3cfa3ad8d1743087b906a071147
peters313 <eric.peters3@gmail.com>
2015-05-05 18:50:09 -0500

    update

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 alert("Welcome to Eric Peters' Prime Academy Resume")
 
 var main = function() {
-  /* Push the body and the nav over by 285px over */
+ // open for image//
+ var main = function() {
   $('.icon-menu').click(function() {
-    $('.menu').animate({
-      left: "0px"
+    $('.photo').animate({
+      right: "0px"
     }, 200);
 
     $('body').animate({
-      left: "285px"
+      right: "285px"
     }, 200);
   });
+
+  //close for image//
+    $('.icon-close').click(function() {
+    $('.menu').animate({
+      right: "-285px"
+    }, 200);
+
+    $('body').animate({
+      right: "0px"
+    }, 200);
+  });
+};
 
 $(document).ready(main);
