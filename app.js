464afaf605eaef1423a2a3d36efd10c79c5bf95f
peters313 <eric.peters3@gmail.com>
2015-05-10 21:09:36 -0500

    1

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 alert("Welcome to Eric Peters' Prime Academy Resume")
 
-function omsPlanner (){
+function bestBuy (){
 	var phrase = prompt("Which job description would you like to view?");
 	if (typeof(phrase) != "omsPlanner"){
 			alert("That is not a valid job description!");
 			return false;
+	} else{ 
+		
 	}
 }
 
-$(document).ready(main);
+(document).ready(main);
